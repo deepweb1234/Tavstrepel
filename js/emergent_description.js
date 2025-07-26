8f3b8a77f634f656920c6a044273c2ad8075b75d
deepweb1234 <tavoaguilar320@gmail.com>
2025-07-26 13:44:18 -0500

    Delete js directory

--- js/emergent_description.js
+++
@@ -1,8 +0,0 @@
-function toggleDescription(button) {
-  // Encuentra el contenedor de la card correspondiente
-  const card = button.closest(".col");
-  const description = card.querySelector(".description-window");
-
-  // Alterna la visibilidad de la descripción
-  description.classList.toggle("active");
-}
